3076a9a74e4af79400ee6954401f1a1bf1c1bb08
iamtaekjun <jtj72272503@gmail.com>
2022-09-04 15:17:20 +0900

    first

---
+++ silver/1026.py
@@ -0,0 +1,14 @@
+#1026 보물(그리디 알고리즘) 수정필요
+N = int(input())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+S = 0
+
+for _ in range(N):
+    minIndexNumberOfAAAA = A.index(min(A))
+    maxIndexNumberOfBBBB = B.index(max(B))
+    S += A[minIndexNumberOfAAAA] * B[maxIndexNumberOfBBBB]
+
+    A.pop(minIndexNumberOfAAAA), B.pop(maxIndexNumberOfBBBB)
+
+print(S)
---
+++ silver/10825.py
@@ -0,0 +1,14 @@
+N = int(input())
+studentInfo = []
+
+for _ in range(N):
+    name, korean, english, math = map(str, input().split())
+    studentInfo.append([name, int(korean), int(english), int(math)])
+
+studentInfo = sorted(studentInfo, key = lambda x : x[0])
+studentInfo = sorted(studentInfo, key = lambda x : x[3], reverse = True)
+studentInfo = sorted(studentInfo, key = lambda x : x[2])
+studentInfo = sorted(studentInfo, key = lambda x : x[1], reverse = True)
+
+for i in range(N):
+    print(studentInfo[i][0])
